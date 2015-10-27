0183aba52a0ed30a58387403449e9278b67b374d
Daniel Pérez Rada <dperezrada@gmail.com>
2015-10-27 15:11:20 -0300

    Adding missing repo (backend) for the deploy And minor fix in the deploy

--- examples/numbers/workers/create_random_number.py
+++ examples/numbers/workers/create_random_number.py
@@ -11,7 +11,7 @@ def process(specific_info, data, *args):
     numbers_range = inputs.get('numbers_range')
     how_many_to_create = inputs.get('how_many_to_create')
 
-    for _ in range(1, how_many_to_create):
+    for _ in range(1, how_many_to_create+1):
         sleep(0.05) #Sleep for visualization purpose
         yield {'number': randint(numbers_range[0], numbers_range[1])}
 
--- examples/numbers/workers/find_even.js
+++ examples/numbers/workers/find_even.js
@@ -5,8 +5,9 @@ var process_func = function(specific_info, data, callback, options){
     try{
         var inputs = poliglo.inputs.get_inputs(data, specific_info);
         var return_values = [];
-        if(inputs.number % 2 === 0)
+        if(inputs.number % 2 === 0){
             return_values = [inputs,];    //is even
+        }
         callback(null, return_values);
     }catch(err){
         callback(err, []);
